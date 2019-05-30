1514c5d810ac48eddf71f3ca334c3be847369233
benrdavison <48724611+benrdavison@users.noreply.github.com>
2019-05-30 10:53:55 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'brd_mod',         # How you named your package folder (MyLib)
   packages = ['brd_mod'],   # Chose the same as "name"
-  version = '0.12',      # Start with a small number and increase it with every change you make
+  version = '0.14',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A small library containing various statistic, econometric, financial, and geospatial functions',   # Give a short description about your library
   author = 'Ben Davison',                   # Type in your name
   author_email = 'benjabee10@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/benrdavison/brd_mod',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/benrdavison/brd_mod/archive/v_012.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/benrdavison/brd_mod/archive/v_014.tar.gz',    # I explain this later on
   keywords = ['statistics', 'economics', 'geospatial', 'probability', 'econometrics', 'finance'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
